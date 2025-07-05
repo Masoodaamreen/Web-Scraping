df1147f3d99b3fd332f6fc7fbd464f0fb2026542
Masoodaamreen <162815031+Masoodaamreen@users.noreply.github.com>
2025-07-05 19:32:41 +0530

    web.py

--- web.py
+++ web.py
@@ -1,11 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url = "https://quotes.toscrape.com"
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-quotes = soup.find_all('span', class_='text')
+
+quotes = soup.find_all('div', class_='quote')
+
+  
+data = []
 
 for quote in quotes:
-    print(quote.text)
+    text = quote.find('span', class_='text').text
+    author = quote.find('small', class_='author').text
+    tags = [tag.text for tag in quote.find_all('a', class_='tag')]
+
+    data.append({
+        'quote': text,
+        'author': author,
+        'tags': tags
+    })
+
+ 
+json_data = json.dumps(data, indent=4)
+print(json_data)
